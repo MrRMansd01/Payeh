0ee46e28dcba73a126108d474b76d0da1d22a117
Mr.Rsd01 <mr.rsd01man@gmail.com>
2025-07-15 21:59:49 +0330

    Update ChatRoom.jsx

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -3,9 +3,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
 import api from '../api';
 import './ChatRoom.css';
-import { supabase } from '../supabaseClient';
 
 // کلاینت سوپابیس برای قابلیت‌های لحظه‌ای (Real-time) و آپلود فایل
+const supabaseUrl = 'https://lholzspyazziknxqopmi.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxob2x6c3B5YXp6aWtueHFvcG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMjc0MTAsImV4cCI6MjA1NzYwMzQxMH0.uku06OF-WapBhuV-A_rJBXu3x24CKKkSTM0SnmPIOOE';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // هوک برای دریافت اطلاعات کاربر فعلی
 const useAuth = () => {
